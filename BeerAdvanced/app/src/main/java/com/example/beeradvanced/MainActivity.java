9e0b29f3d1d3c2729cf28bf38c801de0e9b94314
wojciknatalia <nataliaaawojcik@gmail.com>
2019-08-29 09:13:09 +0200

    First commit

---
+++ BeerAdvanced/app/src/main/java/com/example/beeradvanced/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.beeradvanced;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer (View view) {
+        TextView brands=(TextView) findViewById(R.id.brands);
+    }
+}

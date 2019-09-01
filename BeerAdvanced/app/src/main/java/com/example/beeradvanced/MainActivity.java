2cae3a1d14d389abec4985ec2bd900f8ae746107
wojciknatalia <nataliaaawojcik@gmail.com>
2019-09-01 23:18:00 +0200

    Button is working

--- BeerAdvanced/app/src/main/java/com/example/beeradvanced/MainActivity.java
+++ BeerAdvanced/app/src/main/java/com/example/beeradvanced/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickFindBeer (View view) {
         TextView brands=(TextView) findViewById(R.id.brands);
+        Spinner color=(Spinner)findViewById(R.id.color);
+        String beerType=String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
     }
 }
